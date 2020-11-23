3fa118e5b908ea5ca7702de938f897c5b7bf4dbc
Xerxes Battiwalla <xerxesb@gmail.com>
2020-11-23 14:01:59 +1100

    Sending raw unencoded bytestream over the wire

--- images/image_client.py
+++ images/image_client.py
@@ -11,17 +11,19 @@ socket = context.socket(zmq.REQ)
 # socket.connect("tcp://localhost:5555")
 socket.connect("ipc:///tmp/zmqdemo/0")
 
-window_name = "Python"
+window_name = "Python (ESC to exit)"
 
 def decode_image(encoded_data):
     decoded = base64.b64decode(encoded_data)
-    nparr = np.fromstring(decoded, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    return img
-
+    nparr = np.fromstring(decoded, np.uint8).reshape(480, 640, 3)
+    # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    print(f"Array shape: {nparr.shape}")
+    # print("Img: " + img)
+    return nparr
 
 request = 0
-while True:
+key = 0
+while (key != 27):
     print("Sending request ", request, "...")
     socket.send_string("Hello")
 
@@ -34,5 +36,5 @@ while True:
     cv2.imwrite("py_bin.jpg", image)
     cv2.imshow(window_name, image)
 
-    cv2.waitKey(10)
+    key = cv2.waitKey(10)
     request += 1
--- images/image_server.cpp
+++ images/image_server.cpp
@@ -12,48 +12,89 @@
 
 using namespace cv;
 
-
 //! Write cv::Mat as binary
 /*!
 \param[out] ofs output file stream
 \param[in] out_mat mat to save
 */
-bool writeMatBinary(std::ofstream& ofs, const cv::Mat& out_mat)
+bool writeMatBinary(std::ofstream &ofs, const cv::Mat &out_mat)
 {
-	if(!ofs.is_open()){
-		return false;
-	}
-	if(out_mat.empty()){
-		int s = 0;
-		ofs.write((const char*)(&s), sizeof(int));
-		return true;
-	}
-	int type = out_mat.type();
-	ofs.write((const char*)(&out_mat.rows), sizeof(int));
-	ofs.write((const char*)(&out_mat.cols), sizeof(int));
-	ofs.write((const char*)(&type), sizeof(int));
-	ofs.write((const char*)(out_mat.data), out_mat.elemSize() * out_mat.total());
+    if (!ofs.is_open())
+    {
+        return false;
+    }
+    if (out_mat.empty())
+    {
+        int s = 0;
+        ofs.write((const char *)(&s), sizeof(int));
+        return true;
+    }
+    int type = out_mat.type();
+    ofs.write((const char *)(&out_mat.rows), sizeof(int));
+    ofs.write((const char *)(&out_mat.cols), sizeof(int));
+    ofs.write((const char *)(&type), sizeof(int));
+    ofs.write((const char *)(out_mat.data), out_mat.elemSize() * out_mat.total());
 
-	return true;
+    return true;
 }
-
 
 //! Save cv::Mat as binary
 /*!
 \param[in] filename filaname to save
 \param[in] output cvmat to save
 */
-bool SaveMatBinary(const std::string& filename, const cv::Mat& output){
-	std::ofstream ofs(filename, std::ios::binary);
-	return writeMatBinary(ofs, output);
-}
-
-void SaveVecBinary(const std::string& filename, const std::vector<uchar> data) 
+bool SaveMatBinary(const std::string &filename, const cv::Mat &output)
 {
     std::ofstream ofs(filename, std::ios::binary);
-    ofs.write((char*)&data[0], data.size());
+    return writeMatBinary(ofs, output);
 }
 
+void SaveVecBinary(const std::string &filename, const std::vector<uchar> data)
+{
+    std::ofstream ofs(filename, std::ios::binary);
+    ofs.write((char *)&data[0], data.size());
+}
+
+std::string type2str(int type)
+{
+    std::string r;
+
+    uchar depth = type & CV_MAT_DEPTH_MASK;
+    uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+    switch (depth)
+    {
+    case CV_8U:
+        r = "8U";
+        break;
+    case CV_8S:
+        r = "8S";
+        break;
+    case CV_16U:
+        r = "16U";
+        break;
+    case CV_16S:
+        r = "16S";
+        break;
+    case CV_32S:
+        r = "32S";
+        break;
+    case CV_32F:
+        r = "32F";
+        break;
+    case CV_64F:
+        r = "64F";
+        break;
+    default:
+        r = "User";
+        break;
+    }
+
+    r += "C";
+    r += (chans + '0');
+
+    return r;
+}
 
 int main()
 {
@@ -72,14 +113,13 @@ int main()
     // socket.bind("tcp://*:5555");
     socket.bind("ipc:///tmp/zmqdemo/0");
 
-
     // create a window to display the images from the webcam
     cv::namedWindow("C++", CV_WINDOW_AUTOSIZE);
     cv::Mat frame;
 
     std::cout << "Starting loop..." << std::endl;
     // display frames indefinitely until ESC pressed
-    for(int i = 0; i >= 0; i++)
+    for (int i = 0; i >= 0; i++)
     {
         std::cout << "Server Frame " << i << std::endl;
         // receive a request from client
@@ -89,21 +129,26 @@ int main()
 
         camera >> frame;
         //   if(frame.empty()) break; // end of video stream
-        imshow("C++", frame);
-        SaveMatBinary("cpp_bin.raw", frame);
+        imshow("C++ (ESC to exit)", frame);
+        // SaveMatBinary("cpp_bin.raw", frame);
 
-        std::vector<uchar> jpg;
-        std::vector<int> param(2);
-        param[0] = cv::IMWRITE_JPEG_QUALITY;
-        param[1] = 80;//default(95) 0-100k
-        cv::imencode(".jpg", frame, jpg, param);
-        SaveVecBinary("cpp_bin.jpg", jpg);
+        std::string ty = type2str(frame.type());
+        printf("Matrix: %s %dx%d \n", ty.c_str(), frame.cols, frame.rows);
+        // std::cout << "Mat Type = " << frame.type() << std::endl;
+
+        // std::vector<uchar> jpg;
+        // std::vector<int> param(2);
+        // param[0] = cv::IMWRITE_JPEG_QUALITY;
+        // param[1] = 80;//default(95) 0-100k
+        // cv::imencode(".jpg", frame, jpg, param);
+        // SaveVecBinary("cpp_bin.jpg", jpg);
 
         // Convert frame to serialisable format
-        // std::vector<uchar> array;
-        // array.assign(jpg.data, jpg.data + jpg.total() * jpg.channels());
-        // std::string encodedData = base64_encode(&array[0], array.size());
-        std::string encodedData = base64_encode(&jpg[0], jpg.size());
+        std::vector<uchar> array;
+        array.assign(frame.data, frame.data + frame.total() * frame.channels());
+        std::string encodedData = base64_encode(&array[0], array.size());
+
+        // std::string encodedData = base64_encode(&jpg[0], jpg.size());
 
         std::cout << "Sending payload " << i << std::endl;
         socket.send(zmq::buffer(encodedData));
@@ -112,7 +157,8 @@ int main()
 
         // std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        if(cv::waitKey(10) == 27) break; // stop capturing by pressing ESC
+        if (cv::waitKey(10) == 27)
+            break; // stop capturing by pressing ESC
     }
 
     return 0;
