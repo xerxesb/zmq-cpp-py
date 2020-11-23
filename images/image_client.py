2d72ac85ee9f6646e0d42cd42a26fdfe1a5ae994
Xerxes Battiwalla <xerxesb@gmail.com>
2020-11-23 11:39:09 +1100

    Demo of working CPP/Py transport of images using JPG encoding

--- images/image_client.py
+++ images/image_client.py
@@ -1,10 +1,7 @@
-#
-# Hello World client in Python
-# Connects REQ socket to tcp://localhost:5555
-# Sends "Hello" to server, expects "World" back
-#
-# INSTALLED VIA EASY_INSTALL
 import zmq
+import cv2
+import base64
+import numpy as np
 
 context = zmq.Context()
 
@@ -14,11 +11,28 @@ socket = context.socket(zmq.REQ)
 # socket.connect("tcp://localhost:5555")
 socket.connect("ipc:///tmp/zmqdemo/0")
 
-# Do 10 requests, waiting each time for a response
-for request in range(10):
+window_name = "Python"
+
+def decode_image(encoded_data):
+    decoded = base64.b64decode(encoded_data)
+    nparr = np.fromstring(decoded, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
+
+
+request = 0
+while True:
     print("Sending request ", request, "...")
     socket.send_string("Hello")
 
     # Get the reply.
     message = socket.recv()
-    print("Received reply ", request, "[", message, "]")
+    print("Received reply ", request)
+    # print(message)
+
+    image = decode_image(message)
+    cv2.imwrite("py_bin.jpg", image)
+    cv2.imshow(window_name, image)
+
+    cv2.waitKey(10)
+    request += 1
--- images/image_server.cpp
+++ images/image_server.cpp
@@ -1,11 +1,59 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <fstream>
 #include <iostream>
+#include <chrono>
+#include <thread>
+
+#include <zmq.hpp>
 
 #include "base64.h"
 
 using namespace cv;
+
+
+//! Write cv::Mat as binary
+/*!
+\param[out] ofs output file stream
+\param[in] out_mat mat to save
+*/
+bool writeMatBinary(std::ofstream& ofs, const cv::Mat& out_mat)
+{
+	if(!ofs.is_open()){
+		return false;
+	}
+	if(out_mat.empty()){
+		int s = 0;
+		ofs.write((const char*)(&s), sizeof(int));
+		return true;
+	}
+	int type = out_mat.type();
+	ofs.write((const char*)(&out_mat.rows), sizeof(int));
+	ofs.write((const char*)(&out_mat.cols), sizeof(int));
+	ofs.write((const char*)(&type), sizeof(int));
+	ofs.write((const char*)(out_mat.data), out_mat.elemSize() * out_mat.total());
+
+	return true;
+}
+
+
+//! Save cv::Mat as binary
+/*!
+\param[in] filename filaname to save
+\param[in] output cvmat to save
+*/
+bool SaveMatBinary(const std::string& filename, const cv::Mat& output){
+	std::ofstream ofs(filename, std::ios::binary);
+	return writeMatBinary(ofs, output);
+}
+
+void SaveVecBinary(const std::string& filename, const std::vector<uchar> data) 
+{
+    std::ofstream ofs(filename, std::ios::binary);
+    ofs.write((char*)&data[0], data.size());
+}
+
 
 int main()
 {
@@ -17,38 +65,54 @@ int main()
         return 1;
     }
 
+    // initialize the zmq context with a single IO thread
+    zmq::context_t context{1};
+    // construct a REP (reply) socket and bind to interface
+    zmq::socket_t socket{context, zmq::socket_type::rep};
+    // socket.bind("tcp://*:5555");
+    socket.bind("ipc:///tmp/zmqdemo/0");
+
+
     // create a window to display the images from the webcam
     cv::namedWindow("C++", CV_WINDOW_AUTOSIZE);
-
-    // this will contain the image from the webcam
     cv::Mat frame;
 
-    // display the frame until you press a key
-    // for(;;)
+    std::cout << "Starting loop..." << std::endl;
+    // display frames indefinitely until ESC pressed
+    for(int i = 0; i >= 0; i++)
     {
+        std::cout << "Server Frame " << i << std::endl;
+        // receive a request from client
+        zmq::message_t request;
+        socket.recv(request, zmq::recv_flags::none);
+        std::cout << "Received " << request.to_string() << std::endl;
+
         camera >> frame;
         //   if(frame.empty()) break; // end of video stream
-
         imshow("C++", frame);
+        SaveMatBinary("cpp_bin.raw", frame);
+
+        std::vector<uchar> jpg;
+        std::vector<int> param(2);
+        param[0] = cv::IMWRITE_JPEG_QUALITY;
+        param[1] = 80;//default(95) 0-100k
+        cv::imencode(".jpg", frame, jpg, param);
+        SaveVecBinary("cpp_bin.jpg", jpg);
 
         // Convert frame to serialisable format
-        std::vector<uchar> array;
-        if (frame.isContinuous())
-        {
-            array.assign(frame.data, frame.data + frame.total() * frame.channels());
-        }
-        else
-        {
-            for (int i = 0; i < frame.rows; ++i)
-            {
-                array.insert(array.end(), frame.ptr<uchar>(i), frame.ptr<uchar>(i) + frame.cols * frame.channels());
-            }
-        }
+        // std::vector<uchar> array;
+        // array.assign(jpg.data, jpg.data + jpg.total() * jpg.channels());
+        // std::string encodedData = base64_encode(&array[0], array.size());
+        std::string encodedData = base64_encode(&jpg[0], jpg.size());
 
-        std::string encodedData = base64_encode(&array[0], array.size());
-        std::cout << encodedData << std::endl;
+        std::cout << "Sending payload " << i << std::endl;
+        socket.send(zmq::buffer(encodedData));
 
-        //   if(waitKey(10) == 27) break; // stop capturing by pressing ESC
+        // std::cout << encodedData << std::endl;
+
+        // std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        if(cv::waitKey(10) == 27) break; // stop capturing by pressing ESC
     }
 
     return 0;
