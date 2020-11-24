1515a0ba9378180398d3f2191610f0076e2e5a11
Xerxes Battiwalla <xerxesb@gmail.com>
2020-11-24 17:46:33 +1100

    Added support for a metadata payload (in phosphene format)

--- images/image_client.py
+++ images/image_client.py
@@ -18,6 +18,17 @@ def decode_image(encoded_data):
     print(f"Array shape: {nparr.shape}")
     return nparr
 
+def decode_metadata(encoded_data):
+    def obj(e):
+        o = {}
+        o["id"] = e[0]
+        o["intensity"] = e[1]
+        return o
+    if len(encoded_data) == 0:
+        return []
+    arr = np.frombuffer(encoded_data, np.uint32)
+    return list(map(obj, np.split(arr, len(arr) / 2)))
+
 request = 0
 key = 0
 while (key != 27):
@@ -29,14 +40,15 @@ while (key != 27):
     print("Received reply ", request)
 
     status = frame[0] # The command is in the first frame element
-    metadata = frame[1] # The command is in the first frame element
+    raw_metadata = frame[1] # The command is in the first frame element
     raw_orig_image = frame[2] # The original image is in the second element
     raw_alt_image = frame[3] # The alternate image is in the third element
 
-    print(f"Status from Server: {status}")
-    print(f"Metadata from Server: {metadata}")
-    cv2.imshow(window1_name, decode_image(raw_orig_image))
-    cv2.imshow(window2_name, decode_image(raw_alt_image))
+    if (status == "OK"):
+        print(f"Status from Server: {status}")
+        print(f"Metadata from Server: {decode_metadata(raw_metadata)}")
+        cv2.imshow(window1_name, decode_image(raw_orig_image))
+        cv2.imshow(window2_name, decode_image(raw_alt_image))
 
-    key = cv2.waitKey(10)
-    request += 1
+        key = cv2.waitKey(10)
+        request += 1
